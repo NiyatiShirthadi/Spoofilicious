f1bda54f0ba10679b8a8440bbed1fd7c87e4ec9e
NiyatiShirthadi <44228758+NiyatiShirthadi@users.noreply.github.com>
2019-05-03 19:53:14 +0530

    First Commit

---
+++ Spoofy/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movespeed;
+    public float JumpSpeed;
+    private float x;
+    public bool isGrounded = false;
+
+    void Start()
+    {
+       
+    }
+
+    
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        Movement(x);
+                    
+    }
+
+    private void Movement(float x)
+    {
+        Jump();
+        Vector3 movement = new Vector3(x, 0f, 0f);
+        Debug.Log("movement" + movement);
+        transform.position += movement * Time.deltaTime * movespeed;
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, JumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+
+   
+
+
+}
